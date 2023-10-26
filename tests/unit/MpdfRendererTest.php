cf9047c730c728882c8ec8d400de5e216f4d66c6
medilies <medilies@gmail.com>
2023-10-26 16:48:54 +0100

    from row-bloom

---
+++ tests/unit/MpdfRendererTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use RowBloom\MpdfRenderer\MpdfRenderer;
+use RowBloom\RowBloom\Config;
+use RowBloom\RowBloom\Options;
+use RowBloom\RowBloom\Renderers\RendererFactory;
+use RowBloom\RowBloom\Types\Css;
+use RowBloom\RowBloom\Types\Html;
+
+it('factorize')
+    ->expect(fn () => app()->make(RendererFactory::class)->make(MpdfRenderer::NAME))
+    ->toBeInstanceOf(MpdfRenderer::class);
+
+it('renders and get (basic)')
+    ->with([
+        'example 1' => [
+            'template' => Html::fromString('<h1>Title</h1><p>Bold text</p><div>Normal text</div>'),
+            'css' => Css::fromString('p {font-weight: bold;}'),
+            'options' => app()->make(Options::class),
+            'config' => app()->make(Config::class),
+        ],
+    ])
+    ->expect(function ($template, $css, $options, $config) {
+        return app()->make(RendererFactory::class)->make(MpdfRenderer::NAME)
+            ->render($template, $css, $options, $config)->get();
+    })
+    // ? more assertions
+    ->toBeString();
