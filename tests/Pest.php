35445c6fee25808984b0543c11121deb58bc0c08
medilies <medilies@gmail.com>
2023-10-28 11:57:22 +0100

    cleanup

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,10 +1,1 @@
 <?php
-
-use RowBloom\MpdfRenderer\MpdfRenderer;
-use RowBloom\RowBloom\RowBloomServiceProvider;
-use RowBloom\RowBloom\Support;
-
-app()->make(RowBloomServiceProvider::class)->register();
-app()->make(RowBloomServiceProvider::class)->boot();
-
-app()->make(Support::class)->registerRendererDriver(MpdfRenderer::NAME, MpdfRenderer::class);
